d6b71c7f8849313fde849a994598b21634a30227
Akash Biswas <64808720+akshbswas98@users.noreply.github.com>
2021-12-05 17:38:30 +0530

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+return (
+        <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border=grey-200">
+            <h1>© Kshanik-search(2021- present)</h1>
+            <h3> Made in ❤ memory of my grandfather Kshanik Kumar Biswas(1943-2021) </h3>
+        </div>
+    );
+}
+
