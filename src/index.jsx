93ef384bbeb85ac9a422bdad5a6a5e9ff52134bf
akshbswas98 <biswa.akash.ab@gmail.com>
2025-05-14 00:02:14 +0530

    Remove ResultContextProvider from index.jsx to simplify rendering structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
-import { ResultContextProvider } from './contexts/ResultsContextProvider.jsx';
 import './global.css';
 
 /*    /index.html   200 */
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <ResultContextProvider>
-        <Router>
-            <App />
-        </Router>
-    </ResultContextProvider>
+    <Router>
+        <App />
+    </Router>
 );
